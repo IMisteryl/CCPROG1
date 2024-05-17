c125dd5c5c6c4666cea2c799855e20165e0decc9
Mistery <78303181+IMisteryl@users.noreply.github.com>
2024-05-17 19:25:24 +0800

    simple

--- main.c
+++ main.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 
 int main() {
+  char name, prod;
+  int price, quant, x;
+  
+  printf("Hello! What is the first letter of your name?: \n");
+  scanf("%c", &name);
 
+  printf("What is the first letter of the product?: \n");
+  scanf("%c", &prod);
+
+  printf("How much does it cost?: \n");
+  scanf("%d", &price);
+
+  printf("How many would you like?: \n");
+  scanf("%d", &quant);
+
+  printf("%c", name);
+  
+  for (x = 1; x <= quant; x++){
+  
+    printf("%c %d", prod, price);
+  }
   
   return 0;
 }
