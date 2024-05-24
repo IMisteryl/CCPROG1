fb3b8378ded24e4239327b59476ff30a5735e1e9
Mistery <78303181+IMisteryl@users.noreply.github.com>
2024-05-24 18:22:22 +0800

    base

--- main.c
+++ main.c
@@ -1,27 +1,7 @@
 #include <stdio.h>
 
 int main() {
-  char name, prod;
-  int price, quant, x;
-  
-  printf("Hello! What is the first letter of your name?: ");
-  scanf("%c", &name);
 
-  printf("What is the product?: ");
-  scanf(" %c", &prod);
-
-  printf("How much does it cost?: ");
-  scanf("%d", &price);
-
-  printf("How many would you like?: ");
-  scanf("%d", &quant);
-
-  printf("%c \n", name);
-  
-  for (x = 1; x <= quant; x++){
-  
-    printf("%c %d\n", prod, price);
-  }
   
   return 0;
 }
