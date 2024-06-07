35c3cac66183a268e2fd5e75c4af1b110b22474b
Mistery <78303181+IMisteryl@users.noreply.github.com>
2024-06-07 19:29:57 +0800

    v1

--- main.c
+++ main.c
@@ -1,7 +1,46 @@
 #include <stdio.h>
 
-int main() {
+float CountBudget(float fBudget, float *fAmount){
+  char cChoice;
+  
+	if(*fAmount > fBudget) {
+		printf("OVER BUDGET");
+		
+	} if(*fAmount < fBudget) {
+		printf("This is in budget\n");
+    printf("Would you like to add amount? (Y/N): "); 
+    scanf("%c", &cChoice);
+      if(cChoice == Y){
+        AddAmount(&fAmount);
+      }
+		
+	} if (*fAmount == fBudget) {
+		printf("ENOUGH BUDGET");
+	}
+	
+	return 0;	
+}
+
+float AddAmount(float *fIniAmount){
+  float fNewAmount, fAmount;
+  
+  printf("What is the new amount?: );
+  scanf("&f", fNewAmount);
 
   
-  return 0;
+  
+}
+
+int main(){
+	float fAmount, fBudget;
+	
+	printf("What is the initial amount?: ");
+	scanf(" %f", &fAmount);
+	
+	printf("What is the initial budget?: ");
+	scanf(" %f", &fBudget);
+	
+	CountBudget(fBudget, &fAmount);
+	
+	return 0;
 }
