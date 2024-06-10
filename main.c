77144e22b5522d8bf36c52e37ed40855eb9b7bed
Mistery <78303181+IMisteryl@users.noreply.github.com>
2024-06-11 05:02:36 +0800

    V2
    Final version, Added logic and comments

--- main.c
+++ main.c
@@ -1,46 +1,62 @@
 #include <stdio.h>
 
-float CountBudget(float fBudget, float *fAmount){
-  char cChoice;
+//Checks if the budget is overbudget or in the budget and asks the user if they would like to add
+float CheckBudget(float fBudget, float *fAmount, char *cChoice) {
   
-	if(*fAmount > fBudget) {
-		printf("OVER BUDGET");
+	if(*fAmount >= fBudget) {
+		printf("Overbudget\n");
+		*cChoice = 'N';
 		
 	} if(*fAmount < fBudget) {
 		printf("This is in budget\n");
-    printf("Would you like to add amount? (Y/N): "); 
-    scanf("%c", &cChoice);
-      if(cChoice == Y){
-        AddAmount(&fAmount);
-      }
-		
-	} if (*fAmount == fBudget) {
-		printf("ENOUGH BUDGET");
+
+		//printf("%.2f\n", *fAmount); //(This is to check if the amount has been updated)
+    	printf("Would you like to add amount? (Y/N): "); 
+    	scanf(" %c", cChoice);
 	}
-	
-	return 0;	
+
+	return 0;
 }
 
-float AddAmount(float *fIniAmount){
-  float fNewAmount, fAmount;
+//Asks the user the amount they would like to add and adds it to initial amount
+void AddAmount(float fBudget, float *fIniAmount, char cChoice){
+  float fNewAmount;
   
-  printf("What is the new amount?: );
-  scanf("&f", fNewAmount);
+  printf("How much would you like to add to the amount?: ");
+  scanf(" %f", &fNewAmount);
 
-  
-  
-}
+  *fIniAmount += fNewAmount;
+
+  CheckBudget(fBudget, fIniAmount, &cChoice); //loops back to checking the budget
+
+ 	if (cChoice == 'Y' || cChoice == 'y'){  //loops AddAmount if user chooses to add to amount
+		AddAmount(fBudget, fIniAmount, cChoice);
+	}
+
+	if (cChoice == 'N' || cChoice == 'n'){ //Exits after adding amount if user chooses N
+		printf("Exiting...");
+	}
+} 
 
 int main(){
 	float fAmount, fBudget;
-	
+	char cChoice = 'N';
+
 	printf("What is the initial amount?: ");
 	scanf(" %f", &fAmount);
 	
 	printf("What is the initial budget?: ");
 	scanf(" %f", &fBudget);
 	
-	CountBudget(fBudget, &fAmount);
+	CheckBudget(fBudget, &fAmount, &cChoice);
 	
+	if (cChoice == 'Y' || cChoice == 'y'){  //Initially starts to add amount if user chooses to do so
+		AddAmount(fBudget, &fAmount, cChoice);
+	}
+
+	if (cChoice == 'N' || cChoice == 'n'){ //Exits if user chooses to not add amount
+		printf("Exiting...");
+	}
+
 	return 0;
 }
