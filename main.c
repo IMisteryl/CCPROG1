f5859895d7eb2b2d3e8fcf3ca122a1a18844e42f
Mistery <78303181+IMisteryl@users.noreply.github.com>
2024-06-15 20:39:12 +0800

    Version 3 (Already)
    v1 and v2 were coded in an IDE. v1 had 2 functions (excluding main), v2 had more functions but no logic. V3 has added logic and more functions.

--- main.c
+++ main.c
@@ -1,7 +1,64 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main() {
+void OpenMenu(char *cChoice){
+    printf("Welcome to the Random Numbers Game!\n");
+    printf("[E] Enter the Game\n");
+    printf("[V] View the Current Score\n");
+    printf("[X] Exit the Game\n");
 
-  
-  return 0;
+    printf("Answer: ");
+    scanf(" %c", cChoice);
+    
+}
+
+int GamePlay(){
+    char cName, cChoice;
+
+    printf("Enter the first letter of your name? ");
+    scanf("%c", &cName);
+    
+    printf("Hi %c! Let's start the game!\n", cName);
+    printf("[S] Start the Game\n");
+    printf("[X] Exit the Game\n");
+
+    printf("Enter here: ");
+    scanf("%c", &cChoice);
+
+    return 0;
+}
+
+char ShowLevel(){
+    
+}
+
+int ViewScore(){
+
+
+    return 0;
+}
+
+int RNG(){
+    int num1, num2, upper, lower;
+    lower = 1;
+    upper = 100;
+
+    srand(time(NULL));
+    num1 = (rand() % (upper - lower + 1)) + lower;
+    num2 = (rand() % (upper - lower + 1)) + lower;
+
+    printf("Random Number 1: %d\n", num1);
+    printf("Random Number 2: %d\n", num2);
+    
+    return 0;
+}
+
+int main (){
+    char cName, cChoice;
+
+    OpenMenu(&cChoice);
+    
+
+    return 0;
 }
